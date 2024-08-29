10c4d9b214880ae71395b94fb40a25d2bfd017da
almax35 <almaximys@yandex.ru>
2024-08-30 00:36:43 +0300

    add entity to auth

---
+++ src/main/java/com/example/parser/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.parser.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Table(name = "users")
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
